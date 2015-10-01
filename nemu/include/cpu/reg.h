3db96e0494938a73dad15be06f58007488dcb646
tracer-ics2015 <tracer@njuics.org>
2015-10-01 21:44:19 +0800

    >  compile NEMU 141220000 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  21:44:19 up  2:11,  4 users,  load average: 0.50, 0.34, 0.33 875d81a02b7553da9914e0b58858fc44b2ef892

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -15,7 +15,7 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
-	struct {
+	union {//each GPR includes one of the three tpyes defined below, merged together brings benefits
 		uint32_t _32;
 		uint16_t _16;
 		uint8_t _8[2];
@@ -23,7 +23,18 @@ typedef struct {
 
 	/* Do NOT change the order of the GPRs' definitions. */
 
-	uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+	//uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+	//implementation regulations require gpr arrays and variables remains the same all the time
+	//thus direct defination saves space, and specific variable types should be included
+	#define eax gpr[0]._32
+	#define ecx gpr[1]._32
+	#define edx gpr[2]._32
+	#define ebx gpr[3]._32
+	#define esp gpr[4]._32
+	#define ebp gpr[5]._32
+	#define esi gpr[6]._32
+	#define edi gpr[7]._32
+
 
 	swaddr_t eip;
 
@@ -45,3 +56,6 @@ extern const char* regsw[];
 extern const char* regsb[];
 
 #endif
+
+
+//the scope of define?
