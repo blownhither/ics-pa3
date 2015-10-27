e2eb38fc0f100e53a8926b1a76b9f579549ad180
tracer-ics2015 <tracer@njuics.org>
2015-10-28 03:48:36 +0800

    >  compile NEMU 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  03:48:36 up  6:23,  4 users,  load average: 0.48, 0.46, 0.46 c90547b3deb4bc8bf95b604e9b58271e2716f54

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -112,7 +112,9 @@ static bool make_token(char *e) {
 					tokens[nr_token].type = rules[i].token_type;
 					strncpy(tokens[nr_token].str , substr_start , substr_len); 
 					(tokens[nr_token].str)[substr_len]='\0';
-					nr_token++; 
+					nr_token++;
+					//TODO
+					printf("nr_token = %d" , nr_token); 
 				}
 
 				break;
