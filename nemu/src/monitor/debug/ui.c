a63e56d86dc664c31166eb9de00dc383420a568a
tracer-ics2015 <tracer@njuics.org>
2015-11-12 01:35:43 +0800

    >  compile NEMU 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  01:35:43 up 1 day, 16:32,  6 users,  load average: 0.22, 0.41, 0.38 93ccdee1c56981608085c66cd001b181d864797f

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -82,16 +82,15 @@ static int cmd_info(char *args){
 	if(!strcmp(args,"r")){
 		//eax, ecx, edx, ebx, esp, ebp, esi, edi
 		info_register_overflow_flag = false; 
-		printf("eax\t0x%-x\t0x%-d ( | |AH|AL)\n",cpu.eax,cpu.eax);
-		printf("ebx\t0x%-x\t0x%-d ( | |BH|BL)\n",cpu.ebx,cpu.ebx );
-		printf("ecx\t0x%-x\t0x%-d ( | |CH|CL)\n",cpu.ecx,cpu.ecx );
-		printf("edx\t0x%-x\t0x%-d ( | |DH|DL)\n",cpu.edx,cpu.edx );
-		printf("esp\t0x%-x\t0x%-d ( | |SP   )\n",cpu.esp,cpu.esp );
-		printf("ebp\t0x%-x\t0x%-d ( | |BP   )\n",cpu.ebp,cpu.ebp );
-		printf("esi\t0x%-x\t0x%-d ( | |SI   )\n",cpu.esi , cpu.esi);
-		printf("edi\t0x%-x\t0x%-d ( | |DI   )\n",cpu.edi,cpu.edi );
-		printf("eip\t0x%-x\t0x%-d ( | |IP   )\n",cpu.eip,cpu.eip );
-		if(info_register_overflow_flag)printf("Warning: address out of physical memory were treated as -1.\n\tUse x cmd for details.\n\n"); 	
+		printf("eax\t0x%-x\t%-d ( | |AH|AL)\n",cpu.eax,cpu.eax);
+		printf("ebx\t0x%-x\t%-d ( | |BH|BL)\n",cpu.ebx,cpu.ebx );
+		printf("ecx\t0x%-x\t%-d ( | |CH|CL)\n",cpu.ecx,cpu.ecx );
+		printf("edx\t0x%-x\t%-d ( | |DH|DL)\n",cpu.edx,cpu.edx );
+		printf("esp\t0x%-x\t%-d ( | |SP   )\n",cpu.esp,cpu.esp );
+		printf("ebp\t0x%-x\t%-d ( | |BP   )\n",cpu.ebp,cpu.ebp );
+		printf("esi\t0x%-x\t%-d ( | |SI   )\n",cpu.esi , cpu.esi);
+		printf("edi\t0x%-x\t%-d ( | |DI   )\n",cpu.edi,cpu.edi );
+		printf("eip\t0x%-x\t%-d ( | |IP   )\n",cpu.eip,cpu.eip );
 	}
 	else if(!strcmp(args , "b" )|| !strcmp(args , "w")){
 		print_watchpoint_list(); 
