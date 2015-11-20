96dfb73f3e38b1c71404e9107f4d9d205d66bed6
14307130033-Ma Ziyin <mzy_martin@hotmail.com>
2015-11-20 23:43:34 +0800

    call和push实现 修正了call在用户界面的输出 修正了decode_i立即数的错误

--- nemu/src/cpu/exec/control/push-template.h
+++ nemu/src/cpu/exec/control/push-template.h
@@ -14,7 +14,8 @@ make_helper(pushr){
 	uint8_t reg_num = instr_fetch(eip , 1);
 	reg_num -= 0x50; 
 	strcpy(op_src->str , "push"); 
-	print_asm(str(instr)  "l %s\n" , regsl[check_reg_index(reg_num)]);		// include/cpu/exec/helper.h
+	print_asm(str(instr)  "l $%s\n" , regsl[check_reg_index(reg_num)]);		// include/cpu/exec/helper.h
+	//using print_asm to print instr info
 	reg_l(R_ESP) -= 4; 
 	swaddr_write(reg_l(R_ESP) , 4 , reg_l(reg_num));   
 #ifdef MZYDEBUG
