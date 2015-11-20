8e6412eb50370149ce351e22ad5daf3be92c3557
tracer-ics2015 <tracer@njuics.org>
2015-11-20 22:17:37 +0800

    >  compile NEMU 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  22:17:37 up 21:52,  6 users,  load average: 0.29, 0.41, 0.70 5b36a250b7f9fb3ac357a991cde83244cdc0291a

--- nemu/src/cpu/decode/decode-template.h
+++ nemu/src/cpu/decode/decode-template.h
@@ -30,8 +30,9 @@ make_helper(concat(decode_si_, SUFFIX)) {
 	 * by ``eip''. Interpret the result as an signed immediate, and assign
 	 * it to op_src->simm.
 	 */
-	op_src->simm = instr_fetch(eip , DATA_BYTE); 
-	 
+
+	op_src->simm = (DATA_TYPE_S)instr_fetch(eip , DATA_BYTE); 
+	op_src->val = op_src->simm;  
 	
 
 #ifdef DEBUG
