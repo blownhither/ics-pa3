c9a35317cd8316c5376add5dfab0950b17729deb
tracer-ics2015 <tracer@njuics.org>
2015-11-21 00:10:42 +0800

    >  compile NEMU 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  00:10:42 up 23:45,  6 users,  load average: 0.09, 0.30, 0.31 bac1d3f51e7528fde65e82893a3ea3e4c0667b8

---
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -0,0 +1,18 @@
+#include "cpu/exec/template-start.h"
+
+#define instr test
+
+extern bool parity_check(uint32_t );
+static void do_execute () {
+	eflags.eflags.CF = eflags.eflags.OF = 0;
+	eflags.eflags.ZF = !!op_src->val;
+	eflags.eflags.PF = parity_check(op_src->val);
+	eflags.eflags.SF = MSB(op_src->val);
+}
+
+make_instr_helper(i2r)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+
+
+#include "cpu/exec/template-end.h"
