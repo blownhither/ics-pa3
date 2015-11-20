814c3a3a802da7faacdefa4792e68ddd49aa3086
14307130033-Ma Ziyin <mzy_martin@hotmail.com>
2015-11-21 00:13:21 +0800

    implement test

--- nemu/src/cpu/exec/logic/test-template.h
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -5,7 +5,7 @@
 extern bool parity_check(uint32_t );
 static void do_execute () {
 	eflags.eflags.CF = eflags.eflags.OF = 0;
-	eflags.eflags.ZF = !!op_src->val;
+	eflags.eflags.ZF = !op_src->val;
 	eflags.eflags.PF = parity_check(op_src->val);
 	eflags.eflags.SF = MSB(op_src->val);
 	print_asm_template2();
