bf9683d0d4ba946ddbfbb7fe5d0ad15f2b0bbb0d
tracer-ics2015 <tracer@njuics.org>
2015-11-20 23:12:07 +0800

    >  compile NEMU 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  23:12:07 up 22:47,  7 users,  load average: 0.27, 0.29, 0.31 169076a99a677afb9dfaee61fd22257f7e0e51a

--- nemu/src/cpu/exec/control/push-template.h
+++ nemu/src/cpu/exec/control/push-template.h
@@ -14,7 +14,7 @@ make_helper(pushr){
 	uint8_t reg_num = instr_fetch(eip , 1);
 	reg_num -= 0x50; 
 	//printf("  %x:\t%x\t\t\tpush %s" , eip , reg_num+0x50 , regsl[check_reg_index(reg_num)]); 
-	decode_r_l(eip); 	
+	decode_r_b(eip); 	
 	reg_l(R_ESP) -= 4; 
 	swaddr_write(reg_l(R_ESP) , 4 , reg_l(reg_num));   
 #ifdef MZYDEBUG
