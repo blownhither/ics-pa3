3086e6aeb963522c8eed2cd545aecdedfc5abc7e
tracer-ics2015 <tracer@njuics.org>
2015-11-20 09:27:55 +0800

    >  compile NEMU 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  09:27:55 up  9:03,  6 users,  load average: 0.17, 0.26, 0.30 985ad1c096f9f7c85e988be45e5f5381ee160bc3

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -7,12 +7,11 @@ static void do_execute(){
 	/*TODO ?! why +=
 	//DATA_TYPE temp = reg_w(4);
 	MEM_W(reg_l(4) , cpu.eip); //= swaddr_write	
-	#if DATA_BYTE == 2
-	cpu.eip = (cpu.eip + op_src->val) & 0xffff; 
-*///TODO*/
-	printf("op_src->val is %x , op_dest->val is %x\n" , op_src->val , op_dest->val); 
-#if DATA_BYTE == 4
-	cpu.eip = cpu.eip + op_src->simm; 
+	*/
+#if DATA_BYTE == 2
+	cpu.eip = (cpu.eip + op_src->simm) & 0xffff; 
+#elif DATA_BYTE == 4
+	cpu.eip = cpu.eip + op_src->simm;		//decode_si saves result at simm 
 #endif
 	print_asm_template1(); 
 	return; 
