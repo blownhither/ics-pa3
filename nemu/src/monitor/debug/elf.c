93b400a2972246641db96bb9b01b40ec0958e2bb
tracer-ics2015 <tracer@njuics.org>
2015-12-06 16:31:19 +0800

    >  compile NEMU 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  16:31:19 up  1:10,  5 users,  load average: 0.20, 0.33, 0.39 1ad14efd207cb9e86b38596a6212d6ad37ab049e

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -81,7 +81,7 @@ void load_elf_tables(int argc, char *argv[]) {
 	
 	printf("strtab begin:\n%s\n:end\n" , strtab);
 	for(i=0; i<nr_symtab_entry; i++)
-	printf("%x\t%x\t%x\n" , symtab[i].st_name , symtab[i].st_value , symtab[i].st_info); 
+	printf("%x\t%x\t%x\t%x\n" , symtab[i].st_name , symtab[i].st_value , symtab[i].st_info , symtab[i].st_shndx); 
 	printf("MZYDEBUG!!\n"); 
 	
 	fclose(fp);
