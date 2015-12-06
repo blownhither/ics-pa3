7f830086a8a36c2000cd6d7e52413414ce03f989
tracer-ics2015 <tracer@njuics.org>
2015-12-06 22:23:54 +0800

    >  compile NEMU 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  22:23:54 up  7:02,  3 users,  load average: 0.47, 0.41, 0.41 b67c9a27e605df41a77a896c755a92613725f4e2

--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -3,11 +3,9 @@
 #define instr jmp
 static void do_execute(){
     cpu.eip += op_src->val;    
+	print_asm("jmp 0x%x <offset = 0x%x\n>",cpu.eip,op_src->val);
     return ;
 }
-
-
-
 
 make_instr_helper(si);
 #if DATA_BYTE == 2 || DATA_BYTE ==4
