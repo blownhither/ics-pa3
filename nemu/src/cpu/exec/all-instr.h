d3b6f60a483115f9f7aa3333ef1433baa45364dd
tracer-ics2015 <tracer@njuics.org>
2015-12-06 23:18:55 +0800

    >  compile NEMU 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  23:18:55 up  7:57,  3 users,  load average: 0.41, 0.40, 0.42 49b28ac491972315c104ac9e8075e6739bda0abc

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -11,6 +11,7 @@
 #include "arith/idiv.h"
 #include "arith/div.h"
 #include "arith/sub.h"
+#include "arith/add.h"
 
 #include "binary/cmp.h"
 
---
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -0,0 +1,23 @@
+#include "cpu/exec/template-start.h"
+
+#define instr add
+
+static void do_execute(){
+	DATA_TYPE ans = op_dest->val + op_src->val;
+	OPERAND_W(op_dest,ans);
+	eflags.eflags.ZF = !ans;
+	eflags.eflags.SF = MSB(ans);
+	eflags.eflags.OF = (MSB(ans)!=MSB(op_dest->val)) && (MSB(ans)!=MSB(op_src->val));
+	extern bool parity_check(uint32_t ); 
+	eflags.eflags.PF = parity_check(ans );
+	eflags.eflags.CF = op_src->val > ans;
+}
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+//make_instr_helper(si2rm)
+
+
+#include "cpu/exec/template-end.h"
