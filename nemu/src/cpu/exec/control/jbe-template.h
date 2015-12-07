4c804ab45fa00da5caf8222b5d1ba81ec07c2928
tracer-ics2015 <tracer@njuics.org>
2015-12-07 17:56:55 +0800

    >  compile NEMU 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  17:56:55 up  6:07,  3 users,  load average: 0.49, 0.34, 0.36 82b811195c352c0a79657598e954ff16275c116

--- nemu/src/cpu/exec/control/jbe-template.h
+++ nemu/src/cpu/exec/control/jbe-template.h
@@ -5,7 +5,7 @@ make_helper(concat(jbe_si_ , SUFFIX)){
 	DATA_TYPE_S offset = instr_fetch(eip+1 , DATA_BYTE); 
 	if(eflags.eflags.CF==1 || eflags.eflags.ZF==1){
 #if DATA_BYTE == 1 
-		cpu.eip += offset & 0xffffff00;
+		cpu.eip += offset | 0xffffff00;
 #endif	
 		cpu.eip += offset; 
 	}
--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -3,7 +3,7 @@
 #define instr jmp
 static void do_execute(){
     cpu.eip += op_src->val;    
-	print_asm("jmp 0x%x <offset = 0x%x\n>",cpu.eip,op_src->val);
+	print_asm("jmp 0x%x <offset = 0x%x\n>",cpu.eip+1+DATA_BYTE,op_src->val);
     return ;
 }
 
