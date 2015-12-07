7627544d0031cb7179be4042a892cbf05fc23f57
tracer-ics2015 <tracer@njuics.org>
2015-12-08 00:28:48 +0800

    >  compile NEMU 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  00:28:48 up 12:39,  3 users,  load average: 1.66, 1.32, 0.85 3def0a6596ba99fe598df6fbb2a3ec577de88c8e

--- nemu/src/cpu/exec/control/jbe-template.h
+++ nemu/src/cpu/exec/control/jbe-template.h
@@ -4,14 +4,7 @@ make_helper(concat(jbe_si_ , SUFFIX)){
 
 	DATA_TYPE_S offset = instr_fetch(eip+1 , DATA_BYTE); 
 	if(eflags.eflags.CF==1 || eflags.eflags.ZF==1){
-		//printf("executing\n");
-		//if(offset<0)
-		//cpu.eip += (offset | 0xffffff00);
-		//if(!MSB(offset))
 		cpu.eip += offset;
-		//else
-		//cpu.eip -= ~offset + 1;
- 
 	}
 	print_asm("jbe ""0x%x <offset = 0x%x\n>",cpu.eip+DATA_BYTE+1,offset);
 	return DATA_BYTE + 1;
