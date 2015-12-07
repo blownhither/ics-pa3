25c827c09fda86846e93f8bbdc966ed7b3b804ab
tracer-ics2015 <tracer@njuics.org>
2015-12-07 17:43:51 +0800

    >  compile NEMU 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  17:43:51 up  5:54,  3 users,  load average: 0.34, 0.42, 0.41 4b98ab792db3902f98e2a7b380157ff436e3370a

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -200,12 +200,12 @@ static int cmd_bt(char *args){
 	query_func(cpu.eip,func_name);
 	printf("in %s\n",func_name);
 	while(1){			// ?!
-		printf("asd\n");
+		if(cur_ebp==0)break;
 		cur_eip = swaddr_read(cur_ebp+4,4);
 		//if(cur_eip<=0 || cur_ebp<=0)break;
 		cur_ebp = swaddr_read(cur_ebp,4);
-		printf("ebp%x,eip%x\n",cur_ebp,cur_eip);
-		if(cur_eip<=0 || cur_ebp<=0)break;
+		printf("ebp:%x,eip:%x\n",cur_ebp,cur_eip);
+		
 		
 		
 		
