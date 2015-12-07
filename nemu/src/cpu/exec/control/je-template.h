8a7b6f1d4539ecf036ffa4f2f8afbd742c13024f
tracer-ics2015 <tracer@njuics.org>
2015-12-07 21:41:30 +0800

    >  compile NEMU 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  21:41:30 up  9:52,  3 users,  load average: 0.48, 0.42, 0.48 bc52cdcdc9d297ca18f2f9001b66f3bf40571e7

--- nemu/src/cpu/exec/control/je-template.h
+++ nemu/src/cpu/exec/control/je-template.h
@@ -3,7 +3,7 @@
 #define instr je
 
 
-//#if DATA_BYTE == 4 || DATA_BYTE == 2
+#if DATA_BYTE == 4 || DATA_BYTE == 2
 make_helper(concat(je_i_ , SUFFIX)){
 
 	DATA_TYPE_S offset = instr_fetch(eip+1 , DATA_BYTE); 
@@ -18,6 +18,24 @@ make_helper(concat(je_i_ , SUFFIX)){
 	printf("DATA_BYTE is %x",DATA_BYTE);
 	return DATA_BYTE + 1;	//?!
 }
+#elif DATA_BYTE == 1
+make_helper(je_i_b){
+	DATA_TYPE_S offset = instr_fetch(eip+1 , DATA_BYTE); 
+	printf("in _b offset fetched is %x\n",offset);
+	printf("eip now is %x\n",cpu.eip);	
+	if(eflags.eflags.ZF){
+		cpu.eip += offset; 
+	}
+	printf("eip now is %x\n",cpu.eip);
+	print_asm("je 0x%x <offset = 0x%x\n>",cpu.eip+DATA_BYTE+1,offset);
+	printf("eip now is %x\n",cpu.eip);
+	printf("DATA_BYTE is %x\n",DATA_BYTE);
+	return DATA_BYTE + 1;	//?!
+}
+
+#endif
+
+
 
 #undef instr
 #include "cpu/exec/template-end.h"
