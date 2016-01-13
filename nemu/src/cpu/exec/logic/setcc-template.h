f13c2412660bea376fe5941c9737ccc9207cbcf4
tracer-ics2015 <tracer@njuics.org>
2016-01-13 23:22:37 +0800

    >  compile NEMU 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  23:22:37 up  1:53,  4 users,  load average: 0.38, 0.36, 0.40 f0273be7ccf35e7f14e5e30fd5761ef679da1477

--- nemu/src/cpu/exec/logic/setcc-template.h
+++ nemu/src/cpu/exec/logic/setcc-template.h
@@ -4,13 +4,33 @@
 
 static void do_execute () {
 	
-	uint8_t condition = instr_fetch(cpu.eip-1,1);
-	printf("%x\n",condition);
+	uint8_t condition = instr_fetch(cpu.eip-2,1), ans;
+	switch(condition){
+		case 0x90: ans = eflags.eflags.OF;break;
+		case 0x91: ans = !eflags.eflags.OF;break;
+		case 0x92: ans = eflags.eflags.CF;break;
+		case 0x93: ans = !eflags.eflags.CF;break;
+		case 0x94: ans = eflags.eflags.ZF;break;
+		case 0x95: ans = !eflags.eflags.ZF;break;
+		case 0x96: ans = (eflags.eflags.CF || eflags.eflags.ZF);break;
+		case 0x97: ans = !(eflags.eflags.CF || eflags.eflags.ZF);break;
+		case 0x98: ans = eflags.eflags.SF;break;
+		case 0x99: ans = !eflags.eflags.SF;break;
+		case 0x9a: ans = eflags.eflags.PF;break;
+		case 0x9b: ans = !eflags.eflags.PF;break;
+		case 0x9c: ans = (eflags.eflags.OF != eflags.eflags.SF);break;
+		case 0x9d: ans = (eflags.eflags.OF == eflags.eflags.SF);break;
+		case 0x9e: case 0x9f:ans = (eflags.eflags.ZF || eflags.eflags.SF!=eflags.eflags.OF);break;
+		 //ans = !eflags.eflags.OF;break;
+		default: panic("exception for setcc!\n");
+		
+	}
+	OPERAND_W(op_dest,ans);
 	/* There is no need to update EFLAGS, since no other instructions 
 	 * in PA will test the flags updated by this instruction.
 	 */
 
-//	print_asm_template2();
+	print_asm_template2();
 }
 
 make_instr_helper(rm)
