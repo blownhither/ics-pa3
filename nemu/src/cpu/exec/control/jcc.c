86a118218b5aea602d86f5f7a47da1e80055b576
tracer-ics2015 <tracer@njuics.org>
2016-01-24 16:57:14 +0800

    >  compile NEMU 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  16:57:14 up  3:40,  4 users,  load average: 0.52, 0.42, 0.44 aa76edac8caf2eca056c496b15a815e537330

--- nemu/src/cpu/exec/control/jcc.c
+++ nemu/src/cpu/exec/control/jcc.c
@@ -13,19 +13,19 @@
 #undef DATA_BYTE
 
 
-//make_helper_v(jo_si) //0x0f 80
-//make_helper_v(jno_si) //0x0f 81
-//make_helper_v(jb_si) //0x0f 82
-//make_helper_v(jnb_si) //0x0f 83
+make_helper_v(jo_si) //0x0f 80
+make_helper_v(jno_si) //0x0f 81
+make_helper_v(jb_si) //0x0f 82
+make_helper_v(jnb_si) //0x0f 83
 make_helper_v(je_si) //0x0f 84
 make_helper_v(jne_si) //0x0f 85
-//make_helper_v(jna_si) //0x0f 86
-//make_helper_v(ja_si) //0x0f 87
-//make_helper_v(js_si) //0x0f 88
-//make_helper_v(jns_si) //0x0f 89
-//make_helper_v(jp_si) //0x0f 8a
-//make_helper_v(jpo_si) //0x0f 8b
-//make_helper_v(jl_si) //0x0f 8c
-//make_helper_v(jge_si) //0x0f 8d
-//make_helper_v(jle_si) //0x0f 8e
-//make_helper_v(jnle_si) //0x0f 8f
+make_helper_v(jna_si) //0x0f 86
+make_helper_v(ja_si) //0x0f 87
+make_helper_v(js_si) //0x0f 88
+make_helper_v(jns_si) //0x0f 89
+make_helper_v(jp_si) //0x0f 8a
+make_helper_v(jpo_si) //0x0f 8b
+make_helper_v(jl_si) //0x0f 8c
+make_helper_v(jge_si) //0x0f 8d
+make_helper_v(jle_si) //0x0f 8e
+make_helper_v(jg_si) //0x0f 8f
--- nemu/src/cpu/exec/control/jcc.h
+++ nemu/src/cpu/exec/control/jcc.h
@@ -1,41 +1,41 @@
 #ifndef __JCC_H_
 #define __JCC_H_
 
-//make_helper(jo_si_b); //0x70
-//make_helper(jno_si_b); //0x71
-//make_helper(jb_si_b); //0x72
-//make_helper(jnb_si_b); //0x73
+make_helper(jo_si_b); //0x70
+make_helper(jno_si_b); //0x71
+make_helper(jb_si_b); //0x72
+make_helper(jnb_si_b); //0x73
 make_helper(je_si_b); //0x74
 make_helper(jne_si_b); //0x75
-//make_helper(jna_si_b); //0x76
-//make_helper(ja_si_b); //0x77
-//make_helper(js_si_b); //0x78
-//make_helper(jns_si_b); //0x79
-//make_helper(jp_si_b); //0x7a
-//make_helper(jpo_si_b); //0x7b
-//make_helper(jl_si_b); //0x7c
-//make_helper(jge_si_b); //0x7d
-//make_helper(jle_si_b); //0x7e
-//make_helper(jg_si_b); //0x7f
+make_helper(jna_si_b); //0x76
+make_helper(ja_si_b); //0x77
+make_helper(js_si_b); //0x78
+make_helper(jns_si_b); //0x79
+make_helper(jp_si_b); //0x7a
+make_helper(jpo_si_b); //0x7b
+make_helper(jl_si_b); //0x7c
+make_helper(jge_si_b); //0x7d
+make_helper(jle_si_b); //0x7e
+make_helper(jg_si_b); //0x7f
 
-//make_helper(jcxz_si_b);	//0xe3
+make_helper(jcxz_si_b);	//0xe3
 
-//make_helper(jo_si_v); //0x0f 80
-//make_helper(jno_si_v); //0x0f 81
-//make_helper(jb_si_v); //0x0f 82
-//make_helper(jnb_si_v); //0x0f 83
+make_helper(jo_si_v); //0x0f 80
+make_helper(jno_si_v); //0x0f 81
+make_helper(jb_si_v); //0x0f 82
+make_helper(jnb_si_v); //0x0f 83
 make_helper(je_si_v); //0x0f 84
 make_helper(jne_si_v); //0x0f 85
-//make_helper(jna_si_v); //0x0f 86
-//make_helper(ja_si_v); //0x0f 87
-//make_helper(js_si_v); //0x0f 88
-//make_helper(jns_si_v); //0x0f 89
-//make_helper(jp_si_v); //0x0f 8a
-//make_helper(jpo_si_v); //0x0f 8b
-//make_helper(jl_si_v); //0x0f 8c
-//make_helper(jge_si_v); //0x0f 8d
-//make_helper(jle_si_v); //0x0f 8e
-//make_helper(jg_si_v); //0x0f 8f
+make_helper(jna_si_v); //0x0f 86
+make_helper(ja_si_v); //0x0f 87
+make_helper(js_si_v); //0x0f 88
+make_helper(jns_si_v); //0x0f 89
+make_helper(jp_si_v); //0x0f 8a
+make_helper(jpo_si_v); //0x0f 8b
+make_helper(jl_si_v); //0x0f 8c
+make_helper(jge_si_v); //0x0f 8d
+make_helper(jle_si_v); //0x0f 8e
+make_helper(jg_si_v); //0x0f 8f
 
 
 #endif
