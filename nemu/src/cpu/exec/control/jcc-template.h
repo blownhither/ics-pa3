1b195fcea25b2c75ddebe67545a7d66ce304c2bd
tracer-ics2015 <tracer@njuics.org>
2016-01-24 15:00:58 +0800

    >  compile NEMU 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:00:58 up  1:44,  4 users,  load average: 0.59, 0.50, 0.49 7b944f4473093d24825b29b54d87437429ef878

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -1,7 +1,12 @@
 #include "cpu/exec/template-start.h"
 
 #define JCC_ROUTINE DATA_TYPE_S offs = instr_fetch(cpu.eip+1,DATA_BYTE);\
-                    print_asm(str(instr)" 0x%x <offs=0x%x>\n",cpu.eip+offs+DATA_BYTE+1,offs);
+                    print_asm(str(instr)" 0x%x <offs=0x%x>",cpu.eip+offs+DATA_BYTE+1,offs);
+
+#define JCC_CONDITION(reg) JCC_ROUTINE\
+                            if(reg==1)cpu.eip += offs + DATA_BYTE + 1;\
+                            return 0;
+
 
 //make_helper(jo_si_v); //0x0f 80
 //make_helper(jno_si_v); //0x0f 81
@@ -11,10 +16,11 @@
 #define instr je
 make_helper(concat(je_si_,SUFFIX)){
 	//DATA_TYPE_S offs = instr_fetch(cpu.eip+1,DATA_BYTE);
-    JCC_ROUTINE;
- 	if(eflags.eflags.ZF)cpu.eip += offs + DATA_BYTE + 1;    
+    //JCC_ROUTINE;
+ 	//if(eflags.eflags.ZF)cpu.eip += offs + DATA_BYTE + 1;    
 	//print_asm(str(instr)" 0x%x <offs=0x%x>\n",cpu.eip+offs+DATA_BYTE+1,offs);
-	return 0;
+	//return 0;
+    JCC_CONDITION(eflags.eflags.ZF);
 }
 //make_helper(jne_si_v); //0x0f 85
 #undef instr
