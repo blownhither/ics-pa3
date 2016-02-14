723f4b1804b4895d4aee79eea8705a24dcb23062
tracer-ics2015 <tracer@njuics.org>
2016-02-14 21:45:26 +0800

    >  compile NEMU 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  21:45:26 up  6:38,  3 users,  load average: 0.19, 0.25, 0.30 b5af72786b987e9dcfd6cad9367d64316641d7c1

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -16,7 +16,7 @@ static void do_execute() {
 #if DATA_BYTE==4
 static void do_execute() {
 	uint32_t ans = (op_src->val & (uint32_t)0xff);
-	ans |= ((op_src->val >> 7)&(uint32_t)1)<<31>>15;
+	ans |= ((op_src->val >> 7)&(uint32_t)1) ? 0xffffff00 : 0;
 	printf("src:%x, key:%x, ans:%x, dest:%x\n",op_src->val,(op_src->val>>7)&(uint32_t)1,ans,op_dest->val);
 	OPERAND_W(op_dest, ans);
 	print_asm_template2();
@@ -29,7 +29,7 @@ make_instr_helper(rm)
 #if DATA_BYTE==4
 static void do_execute() {
 	uint32_t ans = (op_src->val & (uint32_t)0xffff);
-	ans |= ((op_src->val >> 15)&(uint32_t)1)<<31>>31;
+	ans |= ((op_src->val >> 15)&(uint32_t)1) ? 0xffff0000 : 0;
 	printf("src:%x, ans:%x, dest:%x\n",op_src->val,ans,op_dest->val);
 	OPERAND_W(op_dest, ans);
 	print_asm_template2();
