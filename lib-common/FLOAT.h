7fc7e73df516b374b01f7116a27c4f7c871ea598
tracer-ics2015 <tracer@njuics.org>
2016-02-21 22:43:46 +0800

    >  run 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  22:43:46 up 10:49,  2 users,  load average: 0.15, 0.22, 0.23 a04e1f46797c6f1292ee70fe1c0f498c08bbea8

--- lib-common/FLOAT.h
+++ lib-common/FLOAT.h
@@ -5,24 +5,42 @@
 
 typedef int FLOAT;
 
+//TODO:
+#define MZYDEBUG 
+
 static inline int F2int(FLOAT a) {
-	nemu_assert(0);
-	return 0;
+	int ret = ((a>>16)&0xffff) | ((a>>31)?0xffff0000:0);//always round lower
+	ret += (a>>15)&1;//round by 0.5
+#ifdef MZYDEBUG
+	printf("F2int(FLOAT 0x%x): 0x%x\n",a,ret);
+#endif
+	return ret;
 }
 
 static inline FLOAT int2F(int a) {
-	nemu_assert(0);
-	return 0;
+	FLOAT ret = a<<16; 	//=== (a<<16) | (a&0x80000000);
+	nemu_assert((a>>15)==0||(a>>15)==0xffffffff);	
+	//the first 17 bits must be same to avoid overflow
+#ifdef MZYDEBUG
+	printf("int2F(int 0x%x): 0x%x\n",a,ret);
+#endif	
+	return ret;
 }
 
 static inline FLOAT F_mul_int(FLOAT a, int b) {
-	nemu_assert(0);
-	return 0;
+	FLOAT ret = a*b;
+#ifdef MZYDEBUG
+	printf("F_mul_int(FLOAT 0x%x, int 0x%x): 0x%x\n",a,b,ret);
+#endif
+	return ret;
 }
 
 static inline FLOAT F_div_int(FLOAT a, int b) {
-	nemu_assert(0);
-	return 0;
+	FLOAT ret = a/b;
+#ifdef MZYDEBUG
+	printf("F_mul_int(FLOAT 0x%x, int 0x%x): 0x%x\n",a,b,ret);
+#endif
+	return ret;
 }
 
 FLOAT f2F(float);
@@ -32,4 +50,4 @@ FLOAT Fabs(FLOAT);
 FLOAT sqrt(FLOAT);
 FLOAT pow(FLOAT, FLOAT);
 
-#endif
+#endif//?0x
