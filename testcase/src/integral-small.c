1644e1e7cdca3c83213242013f231498db2c098d
tracer-ics2015 <tracer@njuics.org>
2016-02-24 16:02:43 +0800

    >  run 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  16:02:43 up 20:11,  2 users,  load average: 1.51, 2.53, 2.46 2feccec15562f6541462570c83f286ada380edf

--- testcase/src/integral-small.c
+++ testcase/src/integral-small.c
@@ -1,9 +1,11 @@
 #include "trap.h"
 #include "FLOAT.h"
-
+FLOAT a;
+int b;
 int main(){
-	FLOAT a = f2F(2.0);
-	int b = F2int(a);
+	a = f2F(2.0);
+	nemu_assert(0x2ffff==a);
+	b = F2int(a);
 	nemu_assert(2==b);
 	HIT_GOOD_TRAP;
 	return 0;
