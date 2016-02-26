d3a5f1e7dcac596d7e8f17f40b83900955ee20d0
tracer-ics2015 <tracer@njuics.org>
2016-02-26 10:56:14 +0800

    >  run 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  10:56:14 up 14 min,  2 users,  load average: 0.24, 0.41, 0.35 7f5cb8846e9e03da30e54bd2570ed489acd6a80c

--- testcase/src/integral-small.c
+++ testcase/src/integral-small.c
@@ -3,8 +3,9 @@
 FLOAT a,c;
 int b;
 int main(){
-	c = int2F(9);
-	nemu_assert(c==0x90000);
+	for(c=-100;c<=0x10000;++c){
+		nemu_assert(int2F(c) == (c<<16));
+	}
 	set_bp();
 	a = f2F(2.0);
 	nemu_assert(0x2ffff==a);
