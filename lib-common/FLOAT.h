6051840bcdbd438194934f583f63ff01cf18d3bf
tracer-ics2015 <tracer@njuics.org>
2016-02-26 10:44:08 +0800

    >  run 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  10:44:08 up 2 min,  2 users,  load average: 1.25, 0.60, 0.23 e17b0af61742ce78e324886dee371c32ab65d547

--- lib-common/FLOAT.h
+++ lib-common/FLOAT.h
@@ -9,7 +9,6 @@ typedef int FLOAT;
 
 static inline int F2int(FLOAT a) {
 	int ret = ((a>>16)&0xffff) | ((a>>31)?0xffff0000:0);//always round lower
-	ret += (a>>15)&1;//round by 0.5
 #ifdef MZYDEBUG
 	printf("F2int(FLOAT 0x%x): 0x%x\n",a,ret);
 #endif
