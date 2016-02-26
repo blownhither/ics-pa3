c0f007549be1ce06ada1c65c5de9d5f4be23ed9d
tracer-ics2015 <tracer@njuics.org>
2016-02-26 14:40:02 +0800

    >  run 14307130033 bh Linux bh-Satellite-P55t-A 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:50:58 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  14:40:02 up  3:57,  2 users,  load average: 0.21, 0.26, 0.33 c443372ecc8d5c0cea8e3ee1e4299d0681f4bafb

--- testcase/src/integral.c
+++ testcase/src/integral.c
@@ -10,18 +10,23 @@ FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
 	int k;
 	FLOAT s,h;
 	h = F_div_int((b - a), n);
+	set_bp();
 	s = F_div_int(fun(a) + fun(b), 2 );
+	set_bp();
 	for(k = 1; k < n; k ++) {
 		s += fun(a + F_mul_int(h, k));
 	}
+	set_bp();
 	s = F_mul_F(s, h);
+	set_bp();
 	return s;
 }
 
 int main() { 
 	FLOAT a = computeT(10, f2F(-1.0), f2F(1.0), f);
+	set_bp();
 	FLOAT ans = f2F(0.551222);
-
+	set_bp();
 	nemu_assert(Fabs(a - ans) < f2F(1e-4));
 	HIT_GOOD_TRAP;
 	return 0;
