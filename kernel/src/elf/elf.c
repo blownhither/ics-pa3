fffb0ce37d57eb72250929434fade6f77fcaa343
tracer-ics2015 <tracer@njuics.org>
2016-03-07 20:19:53 +0800

    >  compile kernel 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  20:19:53 up  3:16,  3 users,  load average: 0.23, 0.24, 0.27 f296690d6595132f757bb1d7b262d449b4d672b

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -31,7 +31,7 @@ uint32_t loader() {
 	elf = (void*)buf;
 	set_bp();
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0x464c457f;
+	const uint32_t elf_magic = 0x7f454c46;//0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 	int i;
