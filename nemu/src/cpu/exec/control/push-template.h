4c64b9cf83b91a84ae58785e0cb7914825451913
tracer-ics2015 <tracer@njuics.org>
2016-03-08 01:36:17 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  01:36:17 up  2:32,  4 users,  load average: 0.22, 0.21, 0.17 c0e72ba86c750f76ba5d6bc6cfe17871d7e1347d

--- nemu/src/cpu/exec/control/push-template.h
+++ nemu/src/cpu/exec/control/push-template.h
@@ -7,7 +7,7 @@ static void do_execute () {
 
 	cpu.esp -= DATA_BYTE;
 	//MEM_W(cpu.esp,op_src->val);
-	swaddr_write(cpu.esp, op_src->val, DATA_BYTE);
+	swaddr_write(cpu.esp, DATA_BYTE, op_src->val);
 	print_asm_template1();
 }
 
