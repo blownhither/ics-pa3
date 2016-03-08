7cefbceb33eaf69cdb9c5d64d97ab5fc08ec9e56
tracer-ics2015 <tracer@njuics.org>
2016-03-08 14:59:01 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  14:59:01 up 34 min,  4 users,  load average: 0.11, 0.14, 0.23 f3ebbc6c94724d0bb76ad6d0374eea21cbfb91fd

--- nemu/src/cpu/exec/string/stos.c
+++ nemu/src/cpu/exec/string/stos.c
@@ -12,11 +12,14 @@ make_helper(stos_b) {
 make_helper(stos_v) {
 	const int ES = 0;	//TODO: check ES
 	if(ops_decoded.is_data_size_16){
-		printf("edi=0x%x, ax=0x%x",cpu.edi, cpu.ax);
+		
 		swaddr_write(ES + cpu.edi, 2, cpu.ax);
 		cpu.edi += eflags.eflags.DF? -2:2;
 	}
 	else{
+
+		printf("edi=0x%x, ax=0x%x",cpu.edi, cpu.ax);
+	
 		swaddr_write(ES + cpu.edi, 4, cpu.eax);
 		cpu.edi += eflags.eflags.DF? -4:4;
 	}
