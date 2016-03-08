cf1c8affae53299cd926b316a59d402387acd703
tracer-ics2015 <tracer@njuics.org>
2016-03-09 01:45:55 +0800

    >  run 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  01:45:55 up  3:14,  3 users,  load average: 0.43, 0.53, 0.37 a4b28f1bb248afe0b83f77715e1f8b79ccf598db

--- testcase/src/hello-str.c
+++ testcase/src/hello-str.c
@@ -16,13 +16,13 @@ void *sbrk(int incr) {
 int main() {
 	sprintf(buf, "%s", "Hello world!\n");
 	nemu_assert(strcmp(buf, "Hello world!\n") == 0);
-
+	set_bp();
 	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
 	nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
-
+	set_bp();
 	sprintf(buf, "%d + %d = %d\n", 2, 10, 12);
 	nemu_assert(strcmp(buf, "2 + 10 = 12\n") == 0);
-
+	
 	HIT_GOOD_TRAP;
 	return 0;
 }
