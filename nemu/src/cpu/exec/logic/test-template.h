61bd12d7c5813f275e132e0b325a5196fa1e3a85
tracer-ics2015 <tracer@njuics.org>
2016-03-10 19:20:44 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  19:20:44 up  5:47,  2 users,  load average: 0.53, 0.24, 0.17 1c4422938057b6cb52117fe6677cdc21ec1e1677

--- nemu/src/cpu/exec/logic/test-template.h
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -5,9 +5,10 @@
 extern bool parity_check(uint32_t );
 static void do_execute () {
 	eflags.eflags.CF = eflags.eflags.OF = 0;
-	eflags.eflags.ZF = !op_src->val;
-	eflags.eflags.PF = parity_check(op_src->val);
-	eflags.eflags.SF = MSB(op_src->val);
+	DATA_TYPE ans = op_src->val & op_dest->val;
+	eflags.eflags.ZF = !ans;
+	eflags.eflags.PF = parity_check(ans);
+	eflags.eflags.SF = MSB(ans);
 	print_asm_template2();
 }
 
