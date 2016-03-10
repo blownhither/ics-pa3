aae987a9d446996db8dfa4353c085708ab6ff790
tracer-ics2015 <tracer@njuics.org>
2016-03-10 21:08:35 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  21:08:35 up  7:35,  2 users,  load average: 0.50, 0.31, 0.22 635e2150aa39ddb8b3363fe7303fef831226e2f7

--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -11,10 +11,11 @@ static void do_execute(){
 make_instr_helper(si);
 #undef instr
 
+/*
 //direct jump
 #define instr jmp	
 static void do_execute(){
-    cpu.eip = op_src->val - op_src->size - 1;	//TODO: check 2    
+    cpu.eip = op_src->val - op_src->size - 1 - (ops_decoded.is_data_size_16?1:0);	//TODO: check 2    
 
 	Log("in jmp_rm, eip=0x%x\n",cpu.eip);
 
@@ -23,6 +24,35 @@ static void do_execute(){
 }
 make_instr_helper(rm);
 #undef instr
+*/
+
+make_helper(concat(jmp_rm_, SUFFIX)) {
+	Log("eip=0x%x to ",eip);
+	concat(decode_rm_, SUFFIX)(eip);
+	cpu.eip = op_src->val;
+	Log("0x%x\n",cpu.eip);
+	return 0;
+}
+/*
+make_helper(jmp_rm_w) {
+	Log("eip=0x%x to ",eip);
+	decode_rm_w(eip);
+	cpu.eip = op_src->val;
+	Log("0x%x\n",cpu.eip);
+	return 0;
+}
+*/
+
+/*
+#define make_instr_helper(type) \
+	make_helper(concat5(instr, _, type, _, SUFFIX)) {
+		return idex(eip, concat4(decode_, type, _, SUFFIX), do_execute);\
+	}
+
+#define do_execute concat4(do_, instr, _, SUFFIX)
+
+idex: exec and return decoded_len+1
+*/
 
 #include "cpu/exec/template-end.h"
 
