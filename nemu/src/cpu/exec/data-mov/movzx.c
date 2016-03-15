c1d4a08c1371a59d1685e67e3acb8a798d24b853
tracer-ics2015 <tracer@njuics.org>
2016-03-16 02:18:07 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  02:18:07 up 20:48,  2 users,  load average: 0.45, 0.47, 0.40 be69f37905e81cd6899fdd987db35f440fb0add

--- nemu/src/cpu/exec/data-mov/movzx.c
+++ nemu/src/cpu/exec/data-mov/movzx.c
@@ -37,7 +37,7 @@ make_helper(movzxl) {
 	len += read_ModR_M(eip+1, op_src, op_dest);
 	
 	op_dest->val = reg_b(op_dest->reg);
-	OPERAND_W(op_dest, op_src->val);
+	OPERAND_W(op_dest, op_src->val&0xff);
 	Log("reg=0x%x,val=0x%x",op_dest->reg,op_src->val);
 	print_asm_template2();
 	return len;
