3b494fd8c6ed22fc474b25aa67588f346d5cc305
tracer-ics2015 <tracer@njuics.org>
2016-03-16 01:57:50 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  01:57:50 up 20:27,  2 users,  load average: 0.60, 0.26, 0.22 31457104debc18e4d495ed343b379d2c15570029

--- nemu/src/cpu/exec/data-mov/movzx.c
+++ nemu/src/cpu/exec/data-mov/movzx.c
@@ -28,6 +28,7 @@ make_helper(movzxl) {
 	else {	
 */							//r/m8 -> r32
 	if(((instr_fetch(eip+1,1)>>6)&3) == 3)op_src->size = 1;
+	else op_src->size = 4;
 	Log("eip=%x",eip+1);
 	int len = read_ModR_M(eip+1, op_src, op_dest);
 	op_dest->val = reg_b(op_dest->reg);
