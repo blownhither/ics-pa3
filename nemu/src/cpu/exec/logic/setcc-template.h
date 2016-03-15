d46fae0cbe2268416820089de5d32c14e1395075
Ma Ziyin <mzy_martin@hotmail.com>
2016-03-15 23:30:15 +0800

    1. fix a typo in cmovcc
    2. fix fatal bug in setcc, where op_src should be used as operand becase a single operand is always regarded as op_src
    
    *. adding -O2 to testcase yield extra bugs
    
    				MA ZIYIN

--- nemu/src/cpu/exec/logic/setcc-template.h
+++ nemu/src/cpu/exec/logic/setcc-template.h
@@ -28,7 +28,7 @@ static void do_execute () {
 		
 	}
 	OPERAND_W(op_src,ans);
-	printf("%s,%d\n",op_src->str, ans);
+	//printf("%s,%d\n",op_src->str, ans);
 	/* There is no need to update EFLAGS, since no other instructions 
 	 * in PA will test the flags updated by this instruction.
 	 */
