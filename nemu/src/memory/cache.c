6f0afdcf4875505de186d74f116a9cefeb6e0420
tracer-ics2015 <tracer@njuics.org>
2016-03-21 22:19:46 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  22:19:46 up 10 min,  2 users,  load average: 0.36, 0.32, 0.24 a9838c642fa2df8f5036565fb9153187667c7a9

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -1,29 +1,16 @@
+#include "cache.h"
+#include "common.h"
+
 typedef unsigned char bool;
 typedef unsigned char uint8_t;
-
-
-#ifndef BLOCK_SIZE
-#define BLOCK_SIZE 64
-#endif
-#ifndef ASSOCT_WAY
-#define ASSOCT_WAY 8
-#endif
-#ifndef BLOCK_NUM
-#define BLOCK_NUM 1024
-#endif
-
-#define LINE_SIZE BLOCK_SIZE/ASSOCT_WAY
-#if LINE_SIZE<=8
-#define LINE_TYPE uint8_t
-#elif LINE_SIZE<=16
-#define LINE_SIZE uint16_t
-#else
-#define LINE_SIZE uint32_t
-#endif
+typedef uint8_t block[BLOCK_SIZE];
 
 typedef struct{
-	LINE_TYPE line[LINE_SIZE];
 	bool valid_bit[ASSOCT_WAY];
-} cache_block;
+	uint32_t tag[ASSOCT_WAY];
+	block data[ASSOCT_WAY][BLOCK_SIZE];
+} cache_group;
 
-cache_block cache[BLOCK_NUM];
+cache_group cache[GROUP_NUM];
+
+
