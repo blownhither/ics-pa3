e8aebbe9acc2381364d89fd9b8658bf761a96508
tracer-ics2015 <tracer@njuics.org>
2016-03-22 10:54:20 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  10:54:20 up  6:37,  2 users,  load average: 0.89, 0.27, 0.20 b738b19739694907f09bbb517034da18cab9b60

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "cpu/reg.h"
 
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
@@ -8,8 +9,13 @@ uint32_t cache_read(hwaddr_t addr, size_t len);
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	uint32_t ret = dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 	uint32_t ret2 = cache_read(addr, len) & (~0u >> ((4 - len) << 3));
-	assert(ret == ret2);
 	printf("dram:0x%x,cache:0x%x\n",ret,ret2);
+	if(ret != ret2) {
+		
+		printf("eip=0x%x,addr=0x%x",cpu.eip, (uint32_t)addr);
+		assert(0);
+	}
+
 	return ret;
 	//return ret2;
 }
