5ddbfdbaaeff90870c5a5698564703afb7d8bafe
tracer-ics2015 <tracer@njuics.org>
2016-03-22 09:20:45 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  09:20:45 up  5:03,  2 users,  load average: 0.44, 0.27, 0.31 4c58344b3cb57fc386b2bf9ae72630ef6fbdc9d2

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -41,8 +41,7 @@ void cache_block_read(hwaddr_t _addr, uint8_t buf[]) {
 	uint32_t index = addr->index;
 	uint32_t offs = addr->offs;
 	uint32_t addr_aligned = *(uint32_t *)addr - offs;
-	printf("_block:addr=0x%x,tag=0x%x,\n\tindex=0x%x,offs=0x%x,addr_align=0x%x\n",
-		*(unsigned int*)addr,tag,index,offs,addr_aligned);
+	//printf("_block:addr=0x%x,tag=0x%x,\n\tindex=0x%x,offs=0x%x,addr_align=0x%x\n",*(unsigned int*)addr,tag,index,offs,addr_aligned);
 	cache_group* group = &cache[index];
 	block *ret_block = NULL;
 	int i, empty_line = -1;
@@ -65,7 +64,7 @@ void cache_block_read(hwaddr_t _addr, uint8_t buf[]) {
 		//read into cache
 		for(i=0; i<BLOCK_SIZE; ++i) {
 			group->data[empty_line][i] = dram_read(addr_aligned + i, 1) & 0xff;	//see memory.c
-			printf("%x ",group->data[empty_line][i]);
+			//printf("%x ",group->data[empty_line][i]);
 			group->tag[empty_line] = tag;
 			group->valid_bit[empty_line] = true;
 		}
@@ -73,9 +72,7 @@ void cache_block_read(hwaddr_t _addr, uint8_t buf[]) {
 	}
 	for(i=0; i<BLOCK_SIZE; ++i) {
 		buf[i] = (* ret_block)[i];
-		//printf("%x ",buf[i]);
 	}
-	printf("\n");
 }
 
 uint32_t cache_read(hwaddr_t addr, size_t len) {
@@ -85,8 +82,6 @@ uint32_t cache_read(hwaddr_t addr, size_t len) {
 	if(((addr + len)&(BLOCK_SIZE - 1)) < (len - 1)) {	//unaligned read
 		cache_block_read(addr + len, buf + BLOCK_SIZE);
 	}
-	
-	//printf("buf:%x\n",buf[offs]);
 	return unalign_rw((buf + offs), 4);
 }
 
