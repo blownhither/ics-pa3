47f1199f47e15babf14dc74ee64d42dd939d4129
tracer-ics2015 <tracer@njuics.org>
2016-03-31 14:57:35 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  14:57:35 up 13:18,  2 users,  load average: 0.24, 0.40, 0.39 6f6e5d4929b49f9fedfc974c8e1d2520f5f2fa08

--- nemu/src/cpu/exec/binary/cmp-template.h
+++ nemu/src/cpu/exec/binary/cmp-template.h
@@ -4,8 +4,8 @@
 
 extern bool parity_check(uint32_t );
 static void do_execute () {
-	if(op_src->type == OP_TYPE_IMM){
-		op_src->val = (DATA_TYPE_S)op_src->imm;
+	if(instr_fetch(cpu.eip,1)==0x83){
+		op_src->val = (op_src->val&0x80)?(0xffffff00|op_src->val):(0xff&op_src->val);
 	}
 	DATA_TYPE_S result = op_dest->val - (DATA_TYPE_S)op_src->val;
 	//!!!note that
