ada81b8af27a25c6dfa0c4bcb3e5f127bbe971a0
tracer-ics2015 <tracer@njuics.org>
2016-03-31 15:13:55 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:13:55 up 13:35,  2 users,  load average: 0.13, 0.24, 0.35 471caa71eeaabd5bd31f1367d0a07a70a4bb1ad8

--- nemu/src/cpu/exec/binary/cmp-template.h
+++ nemu/src/cpu/exec/binary/cmp-template.h
@@ -5,12 +5,7 @@
 extern bool parity_check(uint32_t );
 static void do_execute () {
 	DATA_TYPE_S result;
-	if(instr_fetch(cpu.eip,1)==0x83){
-		//result =op_dest->val - (DATA_TYPE_S)((op_src->simm&0x80)?(0xffffff00|op_src->simm):(0x00ff&op_src->simm));
-		result = op_dest->val - op_src->simm;
-	}
-	else
-		result = op_dest->val - op_src->val;
+	result = op_dest->val - op_src->val;
 	//!!!note that
 	//when "sign-extend is required", please use (DATA_TYPE_S)op_src->imm
 	eflags.eflags.ZF = !result;
