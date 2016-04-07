2a805da2b64c25a2323118193d627a4e2bd94a33
tracer-ics2015 <tracer@njuics.org>
2016-04-08 00:19:40 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  00:19:40 up 38 min,  2 users,  load average: 0.81, 0.73, 0.72 fb47e609d98286860e40b647572bd5a80a181f

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -87,14 +87,14 @@ make_group(group5,
 	inc_rm_v, dec_rm_v, call_rm_v, inv, 
 	jmp_rm_v, inv, push_rm_v, inv)	//TODO:   implement push_m
 
-/* 0x0f 01*/
+/* 0x0f 00*/
 make_group(group6,
-	lgdt, lgdt,  lgdt, lgdt, 
-	lgdt, lgdt,  lgdt, lgdt)
+	inv, inv, inv, inv,
+	inv, inv, inv, inv)
 
-/* 0x0f 02*/
+/* 0x0f 01*/
 make_group(group7,
-	inv, inv, inv, inv, 
+	inv, inv, lgdt, inv, 
 	inv, inv, inv, inv)
 
 
