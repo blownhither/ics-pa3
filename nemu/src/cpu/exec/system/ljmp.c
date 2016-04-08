62fc12c9a7b8371cf02fea69dd152d638c6f9197
Ma Ziyin <mzy_martin@hotmail.com>
2016-04-08 20:22:23 +0800

    ljmp under doubt, study the mannual more

--- nemu/src/cpu/exec/system/ljmp.c
+++ nemu/src/cpu/exec/system/ljmp.c
@@ -17,7 +17,7 @@ make_helper(ljmp){	//0xEA
 			cpu.cs = instr_fetch(cpu.eip+5, 2);
 			cpu.eip = instr_fetch(cpu.eip+1, 4);
 		}
-		print_asm("ljmp %x, %x",cpu.cs, cpu.eip);
+		print_asm("ljmp $0x%x, $0x%x",cpu.cs, cpu.eip);
 	}
 	//TODO: check
 	return 0;
