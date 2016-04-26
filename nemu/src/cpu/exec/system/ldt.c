832692f475937ac3b7a88cb8eee50a79097e0a97
tracer-ics2015 <tracer@njuics.org>
2016-04-27 00:56:19 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  00:56:19 up 16 min,  2 users,  load average: 0.15, 0.28, 0.29 e20d6ada2e89e94e519eead3da428ea2479a706

--- nemu/src/cpu/exec/system/ldt.c
+++ nemu/src/cpu/exec/system/ldt.c
@@ -4,7 +4,7 @@ make_helper(lgdt){	//0F 01 /2
 	lnaddr_t addr = instr_fetch(cpu.eip+3, 4);
 	printf("lgdt: addr=0x%x\n",addr);
 	cpu.gdtr.limit = lnaddr_read(addr, 2);
-	cpu.gdtr.base = instr_fetch(addr + 2, 4);
+	cpu.gdtr.base = lnaddr_read(addr + 2, 4);
 	if(ops_decoded.is_data_size_16){
 		cpu.gdtr.base &= 0xffffff;
 	}
