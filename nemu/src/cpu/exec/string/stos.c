ce46c9752c094d40672761e7d54267aa5b4f4267
tracer-ics2015 <tracer@njuics.org>
2016-04-26 12:15:12 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  12:15:12 up 14:13,  2 users,  load average: 0.74, 0.51, 0.35 2d9fd31da6538c9cf1b89c05b95328ff28fec02b

--- nemu/src/cpu/exec/string/stos.c
+++ nemu/src/cpu/exec/string/stos.c
@@ -4,7 +4,6 @@ make_helper(exec);
 lnaddr_t seg_translate(swaddr_t addr, size_t len, uint8_t cur_segr);
 
 make_helper(stos_b) {
-	//const int ES = 0;	//TODO: check ES
 	lnaddr_t addr = seg_translate(cpu.edi, 1, ES_NUM);
 	lnaddr_write(addr, 1, cpu.al);
 	cpu.edi += eflags.eflags.DF? -1:1;
@@ -12,17 +11,16 @@ make_helper(stos_b) {
 }
 
 make_helper(stos_v) {
-	const int ES = 0;	//TODO: check ES
+	lnaddr_t addr;
 	if(ops_decoded.is_data_size_16){
-		
-		swaddr_write(ES + cpu.edi, 2, cpu.ax);
+		addr = seg_translate(cpu.edi, 2, ES_NUM);
+		lnaddr_write(addr, 2, cpu.ax);
 		cpu.edi += eflags.eflags.DF? -2:2;
 	}
 	else{
 
-		//printf("ecx=0x%x, edi=0x%x, eax=0x%x\n",cpu.ecx, cpu.edi, cpu.eax);
-	
-		swaddr_write(ES + cpu.edi, 4, cpu.eax);
+		addr = seg_translate(cpu.edi, 4, ES_NUM);
+		lnaddr_write(addr, 4, cpu.eax);
 		cpu.edi += eflags.eflags.DF? -4:4;
 	}
 	return 1;
