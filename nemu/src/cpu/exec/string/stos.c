a737004ec2065f50c34401b0dc6aecf33ff6f238
tracer-ics2015 <tracer@njuics.org>
2016-04-26 14:58:34 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  14:58:34 up 16:19,  2 users,  load average: 0.94, 0.47, 0.34 f6454723b3bee4b77d1b2ae6c33fb8cd28c69b92

--- nemu/src/cpu/exec/string/stos.c
+++ nemu/src/cpu/exec/string/stos.c
@@ -22,6 +22,7 @@ make_helper(stos_v) {
 		addr = seg_translate(cpu.edi, 4, ES_NUM);
 		lnaddr_write(addr, 4, cpu.eax);
 		cpu.edi += eflags.eflags.DF? -4:4;
+		printf("stos: addr=0x%x, eax=0x%x\n", addr, cpu.eax);
 	}
 	print_asm("%%eax,%%es:(%%edi)");
 	return 1;
