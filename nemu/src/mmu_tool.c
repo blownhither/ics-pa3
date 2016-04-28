3ae5ce158d756dd792081b212845679402f1055e
tracer-ics2015 <tracer@njuics.org>
2016-04-28 20:23:38 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  20:23:38 up  6:41,  2 users,  load average: 0.25, 0.29, 0.31 7ec01977129cfa19741114c4933671bfc804b61c

--- nemu/src/mmu_tool.c
+++ nemu/src/mmu_tool.c
@@ -63,11 +63,11 @@ hwaddr_t page_translate(lnaddr_t addr) {
 	PDE pde;
 	PTE pte;
 	vpn.val = addr;
-	pde.val = hwaddr_read((cpu.cr3.base << 12) + (vpn.pi << 2), 4);
-	printf("addr=0x%x 0x%x->pde=0x%x \n",addr,(cpu.cr3.base << 12) + (vpn.pi << 2),pde.val);
-	Assert(pde.present,"PDE not present(0x%x) at 0x%x when accessing lnaddr 0x%x\n",pde.val,(cpu.cr3.base << 12) + (vpn.pi << 2),addr);
-	pte.val = hwaddr_read((pde.page_frame << 12) + (vpn.pt << 2), 4);
-	printf("\t0x%x->pte=0x%x ans=0x%x\n",(pde.page_frame << 12) + (vpn.pt << 2),pte.val, (pte.page_frame << 12) + vpn.offset);
-	Assert(pte.present,"PTE not present(0x%x) at 0x%x when accessing lnaddr 0x%x\n",pte.val,(pde.page_frame << 12) + (vpn.pt << 2),addr);
+	pde.val = hwaddr_read((cpu.cr3.base << 12) + (vpn.pi * 4), 4);
+	printf("addr=0x%x 0x%x->pde=0x%x \n",addr,(cpu.cr3.base << 12) + (vpn.pi * 4),pde.val);
+	Assert(pde.present,"PDE not present(0x%x) at 0x%x when accessing lnaddr 0x%x\n",pde.val,(cpu.cr3.base << 12) + (vpn.pi * 4),addr);
+	pte.val = hwaddr_read((pde.page_frame << 12) + (vpn.pt * 4), 4);
+	printf("\t0x%x->pte=0x%x ans=0x%x\n",(pde.page_frame << 12) + (vpn.pt * 4),pte.val, (pte.page_frame << 12) + vpn.offset);
+	Assert(pte.present,"PTE not present(0x%x) at 0x%x when accessing lnaddr 0x%x\n",pte.val,(pde.page_frame << 12) + (vpn.pt * 4),addr);
 	return (pte.page_frame << 12) + vpn.offset;
 }
