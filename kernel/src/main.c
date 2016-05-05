b20ddaeef23b0e8750ee79d45c01f81a6e04a46f
tracer-ics2015 <tracer@njuics.org>
2016-05-05 21:32:19 +0800

    >  compile kernel 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  21:32:19 up  5:14,  3 users,  load average: 0.59, 0.15, 0.11 452c022921836654852913ba403acad7583ed331

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -26,7 +26,7 @@ void init() {
 	/* We must set up kernel virtual memory first because our kernel thinks it 
 	 * is located at 0xc0100000, which is set by the linking options in Makefile.
 	 * Before setting up correct paging, no global variable can be used. */
-	x = 1;
+	//x = 1;
 	init_page();
 
 	/* After paging is enabled, transform %esp to virtual address. */
