fe49311900a6e443681f2186d2da9eec3b0798d8
tracer-ics2015 <tracer@njuics.org>
2016-05-26 17:03:03 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  17:03:03 up 57 min,  4 users,  load average: 0.59, 0.47, 0.47 48a6f2b13f25d8a1ae471fb57b0f557349858614

--- nemu/src/cpu/exec/system/ldt-template.h
+++ nemu/src/cpu/exec/system/ldt-template.h
@@ -5,6 +5,12 @@
 static void do_execute(){
 	printf("op_src->val=%x\teax=%x\top_dest->val=%x\n",op_src->val,cpu.eax,op_dest->val);
 	printf("eip=%x\n",cpu.eip);
+	
+	cpu.gdtr.base = op_dest->val;
+	if(ops_decoded.is_data_size_16){
+		cpu.gdtr.base &= 0xffffff;
+	}
+	print_asm("lgdt %x",op_dest->val);
 }
 
 make_instr_helper(rm_imm)
