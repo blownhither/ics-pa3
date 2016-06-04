9b0d7f8ed8b2bdea4b62e9d4b1babf0b6a3cffbb
tracer-ics2015 <tracer@njuics.org>
2016-06-04 21:05:46 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  21:05:46 up  2:02,  3 users,  load average: 0.28, 0.31, 0.39 6117203ecbdb023eb6aa4dadad35db9ce487ea90

--- nemu/src/cpu/exec/system/int-template.h
+++ nemu/src/cpu/exec/system/int-template.h
@@ -3,9 +3,9 @@
 #define instr int
 
 static void do_execute(){
-	//cpu.eip += 1;	// no need to +1 in template
+	cpu.eip += 2;	// no need to +1 in template
 	print_asm("int %x", op_src->val);
-	printf("int %d",op_src->val);
+	printf("int %d\n",op_src->val);
 	raise_intr(op_src->val);
 }
 
