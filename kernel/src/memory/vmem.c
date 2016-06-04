fce13e45e44c4a48226ffa37c7065324314319ff
tracer-ics2015 <tracer@njuics.org>
2016-06-04 23:13:00 +0800

    >  compile kernel 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  23:13:00 up 58 min,  2 users,  load average: 0.16, 0.18, 0.24 c53d3dca48899a694330bf8efc40d519aaac285

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -33,6 +33,7 @@ void create_video_mapping() {
 void video_mapping_write_test() {
 	int i;
 	uint32_t *buf = (void *)VMEM_ADDR;
+	Log("tested\n");
 	for(i = 0; i < SCR_SIZE / 4; i ++) {
 		buf[i] = i;
 	}
