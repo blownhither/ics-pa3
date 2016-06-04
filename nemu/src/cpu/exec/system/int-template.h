cb02cd6100197a1a1f2d5de4a8c470e2539dea8c
Ma Ziyin <mzy_martin@hotmail.com>
2016-06-04 21:07:07 +0800

    Fix bug in int-template: did not return , so must mannually move eip

--- nemu/src/cpu/exec/system/int-template.h
+++ nemu/src/cpu/exec/system/int-template.h
@@ -3,10 +3,11 @@
 #define instr int
 
 static void do_execute(){
-	cpu.eip += 2;	// no need to +1 in template
+	cpu.eip += 2;
 	print_asm("int %x", op_src->val);
-	printf("int %d\n",op_src->val);
+	//printf("int %d\n",op_src->val);
 	raise_intr(op_src->val);
+	// did not return, so must add eip mannually
 }
 
 make_instr_helper(i)
