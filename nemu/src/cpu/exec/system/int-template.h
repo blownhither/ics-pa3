9a7906047243b519ab1d5c848b25a4c3b0976546
tracer-ics2015 <tracer@njuics.org>
2016-06-05 00:37:59 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  00:37:59 up  2:23,  4 users,  load average: 0.48, 0.56, 0.50 fde5ee6e34c6f7de4504c53e5c23ee92eaacfd11

--- nemu/src/cpu/exec/system/int-template.h
+++ nemu/src/cpu/exec/system/int-template.h
@@ -6,7 +6,8 @@ static void do_execute(){
 	//cpu.eip += 2;			// keep the value of eip, but in raise_intr must +2?
 	print_asm("int %x", op_src->val);
 	//printf("int %d\n",op_src->val);
-	raise_intr(op_src->val, 2);
+	int len = get_instr_len();
+	raise_intr(op_src->val, len);
 }
 
 make_instr_helper(i)
