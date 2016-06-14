2672828b324beb6033a2bd8904ab2b87f1459da2
tracer-ics2015 <tracer@njuics.org>
2016-06-14 14:10:58 +0800

    >  compile NEMU 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  14:10:58 up  3:07,  3 users,  load average: 1.39, 1.35, 0.91 e2cd0f7384a5b2c8528938b0f0f379b7ae806b1a

--- game/src/common/lib/syscall.c
+++ game/src/common/lib/syscall.c
@@ -6,6 +6,7 @@ int __attribute__((__noinline__))
 syscall(int id, ...) {
 	int ret;
 	int *args = &id;
+//	Log("%x %x %x %x", args[0], args[1], args[2], args[3]);	
 	asm volatile("int $0x80": "=a"(ret) : "a"(args[0]), "b"(args[1]), "c"(args[2]), "d"(args[3]));
 	return ret;
 }
@@ -16,10 +17,15 @@ void _exit(int status) {
 
 int open(const char *pathname, int flags) {
 	return syscall(SYS_open, pathname, flags);
+	nemu_assert(0);
+	return 0; 
 }
 
 int read(int fd, char *buf, int len) {
+//	Log("%x %x %x", fd, (int)buf, len);	
 	return syscall(SYS_read, fd, buf, len);
+	nemu_assert(0);
+	return 0; 
 }
 
 int write(int fd, char *buf, int len) {
@@ -28,6 +34,8 @@ int write(int fd, char *buf, int len) {
 
 off_t lseek(int fd, off_t offset, int whence) {
 	return syscall(SYS_lseek, fd, offset, whence);
+	nemu_assert(0);
+	return 0; 
 }
 
 void *sbrk(int incr) {
@@ -47,6 +55,8 @@ void *sbrk(int incr) {
 
 int close(int fd) {
 	return syscall(SYS_close, fd);
+	nemu_assert(0);
+	return 0; 
 }
 
 int fstat(int fd, struct stat *buf) {
