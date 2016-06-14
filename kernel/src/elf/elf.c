2bc62b5c8d96916672830bb4288c9ede668102a4
tracer-ics2015 <tracer@njuics.org>
2016-06-14 15:13:52 +0800

    >  compile kernel 14307130033 bh Linux bh-VirtualBox 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:13:52 up  4:10,  3 users,  load average: 1.30, 0.81, 0.80 768c7f2a174500b714fcfd06574bebd34d0d8d5c

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -2,7 +2,6 @@
 #include "memory.h"
 #include <string.h>
 #include <elf.h>
-//#include <stdio.h>
 
 #define ELF_OFFSET_IN_DISK 0
 
@@ -12,10 +11,7 @@ void ide_read(uint8_t *, uint32_t, uint32_t);
 void ramdisk_read(uint8_t *, uint32_t, uint32_t);
 #endif
 
-#define HEAD_SIZE 4096
 #define STACK_SIZE (1 << 20)
-#define _SIZE_ (1 << 20)
-//0xa0000 - 1
 
 void create_video_mapping();
 uint32_t get_ucr3();
@@ -24,55 +20,41 @@ uint32_t loader() {
 	Elf32_Ehdr *elf;
 	Elf32_Phdr *ph = NULL;
 
-	uint8_t buf[HEAD_SIZE];
-	uint8_t buf_[_SIZE_];
-
+	uint8_t buf[4096];  
 #ifdef HAS_DEVICE
-	ide_read(buf, ELF_OFFSET_IN_DISK, HEAD_SIZE);
+	ide_read(buf, ELF_OFFSET_IN_DISK, 4096);
 #else
-	ramdisk_read(buf, ELF_OFFSET_IN_DISK, HEAD_SIZE);
+	ramdisk_read(buf, ELF_OFFSET_IN_DISK, 4096);
 #endif
-	elf = (void*)buf;
 
-	/* TODO: fix the magic number with the correct one */
+	elf = (void*)buf;
 	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
-
-	int cnt;
+	
+	int i;
 	/* Load each program segment */
-	for(cnt = 0; cnt < elf->e_phnum; ++ cnt) {
+	ph = (void *)buf + elf->e_phoff;
+	for(i=0; i < elf->e_phnum ; i++) {	//e_phnum counts program headers
 		/* Scan the program header table, load each segment into memory */
-/*
-#ifdef HAS_DEVICE
-		ide_read(buf_, ELF_OFFSET_IN_DISK + elf->e_ehsize + cnt * elf->e_phentsize, elf->e_phentsize);
-#else
-		ramdisk_read(buf_, ELF_OFFSET_IN_DISK + elf->e_ehsize + cnt * elf->e_phentsize, elf->e_phentsize);
-#endif
-*/
-		ph = (void*)(buf + elf->e_ehsize + cnt * elf->e_phentsize);
-//		ph = (void*)buf_;
-		if(ph->p_type == PT_LOAD) {
-			/* TODO: read the content of the segment from the ELF file 
+		if(ph->p_type == PT_LOAD) {	//PT_LOAT=1, Loadable program segment 
+			/*  read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-#ifdef HAS_DEVICE
-		ide_read(buf_, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
-#else
-		ramdisk_read(buf_, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
-#endif		
-			
-			uint32_t hwaddr = 
-				mm_malloc(ph->p_vaddr, ph->p_memsz);
+			uint32_t paddr = ph->p_vaddr; 
+#ifdef IA32_PAGE 
+			paddr = mm_malloc(ph->p_vaddr, ph->p_memsz); 
+#endif
 
-			memcpy((void *)hwaddr, (void *)(buf_), ph->p_filesz);
+#ifdef HAS_DEVICE
+			ide_read((void *)paddr, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
+#else
+			ramdisk_read((void *)paddr, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
+#endif
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			
-//			Log("%x %x %x %x", ph->p_vaddr, ph->p_filesz, ph->p_memsz, ph->p_vaddr + ph->p_filesz);
-			memset((void *)hwaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
-
+			memset((void *)paddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
 			extern uint32_t brk;
@@ -80,19 +62,19 @@ uint32_t loader() {
 			if(brk < new_brk) { brk = new_brk; }
 #endif
 		}
+		ph ++;		
 	}
 
 	volatile uint32_t entry = elf->e_entry;
-
 #ifdef IA32_PAGE
 	mm_malloc(KOFFSET - STACK_SIZE, STACK_SIZE);
 
 #ifdef HAS_DEVICE
 	create_video_mapping();
 #endif
+
 	write_cr3(get_ucr3());
 #endif
 
-//	HIT_GOOD_TRAP;
 	return entry;
 }
